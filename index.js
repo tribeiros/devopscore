86e6450954297e7a259c6a1b62475487ebf7f532
Tiago Ribeiro <tribeiros@indracompany.com>
2019-04-05 15:30:13 -0300

    tetris

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
     res.render('index');
